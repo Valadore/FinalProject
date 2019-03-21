c0d39d17d1be7b14811ae4e92684325201c24c2f
phil <Philip.edwards@students.plymouth.ac.uk>
2019-03-21 19:08:15 +0000

    added more activities

--- app/src/main/java/com/example/finalproject/BarcodeScan.java
+++ app/src/main/java/com/example/finalproject/BarcodeScan.java
@@ -1,24 +1,22 @@
 package com.example.finalproject;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class DownloadManifests extends AppCompatActivity {
+public class BarcodeScan extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_download_manifests);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_barcode_scan);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,14 +24,7 @@ public class DownloadManifests extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-        Button button = (Button) findViewById(R.id.button_continue);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent myIntent = new Intent(DownloadManifests.this, BarcodeScan.class);
-                startActivity(myIntent);
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
