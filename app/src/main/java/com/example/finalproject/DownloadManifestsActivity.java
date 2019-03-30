172ad9b0c1f60cd611538e7e7c35bc244f195f7e
phil <Philip.edwards@students.plymouth.ac.uk>
2019-03-30 19:42:05 +0000

    added json manifests, barcode scanner, updated layouts

--- app/src/main/java/com/example/finalproject/DownloadManifestsActivity.java
+++ app/src/main/java/com/example/finalproject/DownloadManifestsActivity.java
@@ -2,14 +2,20 @@ package com.example.finalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DownloadManifestsActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +32,30 @@ public class DownloadManifestsActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String[] manifests = getManifestList();
+        List<String> stringList = new ArrayList<String>(Arrays.asList(manifests));
+
+        //instantiate custom adapter
+        ManifestListAdapter adapter = new ManifestListAdapter((ArrayList<String>) stringList, this);
+
+        //handle listview and assign adapter
+        ListView lView = (ListView)findViewById(R.id.manifest_list);
+        lView.setAdapter(adapter);
+
     }
 
-    //we are going to eventually download manifests from the server
-
-    //for now we can read from local folder
-
-    //need names here to select what ones to download
-    //then download and add to file structure
-
-
+    //temp using local files rather than rest api
+    private String[] getManifestList()
+    {
+        String[] f = null;
+        try {
+            f = getAssets().list("manifests");
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return f;
+    }
 }
---
+++ app/src/main/java/com/example/finalproject/ManifestListAdapter.java
@@ -0,0 +1,67 @@
+package com.example.finalproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ManifestListAdapter extends BaseAdapter implements ListAdapter {
+    private ArrayList<String> list = new ArrayList<String>();
+    private Context context;
+
+
+
+    public ManifestListAdapter(ArrayList<String> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return list.get(pos);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.manifest_list, null);
+        }
+
+        //Handle TextView and display string from your list
+        TextView listItemText = (TextView)view.findViewById(R.id.list_item_string);
+        listItemText.setText(list.get(position));
+
+        //Handle buttons and add onClickListeners
+        Button deleteBtn = (Button)view.findViewById(R.id.download_btn);
+
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //do something
+                list.remove(position); //or some other task
+                notifyDataSetChanged();
+            }
+        });
+
+        return view;
+    }
+}
