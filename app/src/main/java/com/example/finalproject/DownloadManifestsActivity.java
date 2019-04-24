f6127b1c7fa5552537a7bcb25b56000e179a79da
phil <Philip.edwards@students.plymouth.ac.uk>
2019-04-24 19:16:40 +0100

    converting json to usefull stuff

--- app/src/main/java/com/example/finalproject/DownloadManifestsActivity.java
+++ app/src/main/java/com/example/finalproject/DownloadManifestsActivity.java
@@ -10,7 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -43,16 +48,15 @@ public class DownloadManifestsActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.button_continue);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                List<String> manifests = adapter.getManifests();
 
-                //This is where we build the manifest database
+                //send a list of manifests to download
+                List<String> manifests = adapter.getManifests();
                 buildManifests(manifests);
 
                 Intent myIntent = new Intent(DownloadManifestsActivity.this, BarcodeScanActivity.class);
                 startActivity(myIntent);
             }
         });
-
     }
 
     //temp using local files rather than rest api
@@ -63,9 +67,66 @@ public class DownloadManifestsActivity extends AppCompatActivity {
             f = getAssets().list("manifests");
         }
         catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         return f;
+    }
+
+    //This is where we build the manifest database
+    private void buildManifests( List<String> manifests)
+    {
+        //for each manifest lets 'download'
+        for (String manifest : manifests) {
+
+            try {
+                JSONObject manifestObj = new JSONObject(readJSONFromAsset(manifest));
+                Log.d("test", String.valueOf(manifestObj.names()));
+                int size1 = manifestObj.names().length();
+                //get each key
+                for (int i=0; i<size1; i++)
+                {
+                    //get each list of parcels
+                    JSONArray jsonData = manifestObj.getJSONArray((String) manifestObj.names().get(i));
+                    int size2 = jsonData.length();
+                    for (int y=0; y<size2; y++)
+                    {
+                        //address
+                        Log.d("test", String.valueOf(manifestObj.names().get(i)));
+                        //details
+                        Log.d("test", String.valueOf(jsonData.get(y)));
+                    }
+                    //address
+                    //Log.d("test", String.valueOf(manifestObj.names().get(i)));
+                    //details
+                   // Log.d("test", String.valueOf(jsonData));
+
+                    //manifestObj.names().get(i);
+
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+    //For the debug app only, we read the json from a file
+    //If this was a live version this is where we would download
+    public String readJSONFromAsset(String manifest) {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("manifests/" + manifest);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
